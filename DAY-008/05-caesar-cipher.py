2a5d43798c0e2c65a407a836324ad3d6e64e9cf7
Ankush <ankushgt25@gmail.com>
2023-07-23 15:01:27 +0530

    updated

--- DAY-008/05-caesar-cipher.py
+++ DAY-008/05-caesar-cipher.py
@@ -5,3 +5,6 @@ Julius Caesar :)
 
 """)
 
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
