92f4228238bac44540fb61b5f9fef68425c71114
Ankush <ankushgt25@gmail.com>
2023-07-28 10:34:29 +0530

    tiwaribro

---
+++ DAY-010/01-func-with-output.py
@@ -0,0 +1,14 @@
+# def fuction_my():
+#     return 3*2
+
+# output= fuction_my()
+# print(output)
+
+
+# title( function )
+
+def format_name(f_name,l_name):
+    formatted_fname=(f_name.title())
+    formatted_lname=(l_name.title())
+    return f"{formatted_fname} {formatted_lname}"
+print(format_name("AnKuSh", "tiwARI"))
---
+++ DAY-010/02-multiple-returns.py
@@ -0,0 +1,10 @@
+
+
+def format_name(f_name,l_name):
+    if f_name=="" or l_name=="":
+        return "please provide a valid input."
+    formatted_fname=(f_name.title())
+    formatted_lname=(l_name.title())
+    return f"{formatted_fname} {formatted_lname}"
+
+print(format_name(input("What is your first name??\n"),input("What is your last name??\n")))
